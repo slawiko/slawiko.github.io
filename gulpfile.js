083cdc8881a679f247eeeab2dd75945d07c2f7f8
slawiko <shchaurouski.slava@gmail.com>
2016-04-03 22:18:14 +0300

    gulpfile added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 'use strict';
 
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
-gulp.task('default') {
 
-}
+gulp.task('styles', function() {
+	return  gulp.src('develop/stylesheets/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('stylesheets'));
+});
+
+gulp.watch('develop/stylesheets/*', gulp.series('styles'));
+
+gulp.task('serve', function() {
+	browserSync.init({
+		server: '.'
+	});
+});
