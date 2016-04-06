e66128f044ac7bb30ed8132834131ba1cf23ee58
slawiko <shchaurouski.slava@gmail.com>
2016-04-06 20:39:24 +0300

    assets folder added; gulp tasks for assets folder added

--- gulpfile.js
+++ gulpfile.js
@@ -22,8 +22,12 @@ gulp.task(function clean_scripts() {
 	return del(pubDir + '/js');
 });
 
+gulp.task(function clean_assets() {
+	return del(pubDir + '/assets');
+});
+
 gulp.task(function styles() {
-	return gulp.src(['scss/*.scss'])
+	return gulp.src(['_scss/*.scss'])
 		.pipe(debug())
 		.pipe(sass())
 		.pipe(gulp.dest('css'));
@@ -35,33 +39,43 @@ gulp.task(function scripts() {
 		.pipe(gulp.dest(pubDir + '/js'));
 });
 
+gulp.task(function assets() {
+	return gulp.src(['assets/*.*'])
+		.pipe(debug())
+		.pipe(gulp.dest(pubDir + '/assets'));
+});
+
 gulp.task(function watch() {
-	gulp.watch('scss/*.scss', gulp.series(
-		'clean_styles',
-		'styles'
+	gulp.watch('_scss/*.scss', gulp.series(
+		'styles',
+		'jekyll'
 	));
 	gulp.watch('js/*.js', gulp.series(
 		'clean_scripts',
 		'scripts'
 	));
+	gulp.watch('assets/*.*', gulp.series(
+		'clean_assets',
+		'assets'
+	));
 	gulp.watch('./*.html', gulp.series(
 		'jekyll'
 	));
+
+	browserSync.watch(pubDir).on('change', browserSync.reload);
 });
 
 gulp.task(function browser_sync() {
 	browserSync.init({
 		server: pubDir
 	});
-
-	browserSync.watch(pubDir).on('change', browserSync.reload);
 });
 
 gulp.task('jekyll', shell.task(['jekyll build']));
 
 gulp.task('build', gulp.series(
 	'clean',
-	'jekyll',
 	'styles',
+	'jekyll',
 	gulp.parallel('browser_sync', 'watch')
 ));
